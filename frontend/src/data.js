6140f4e68d360a39a3d1c10e90d10fc17f0e3dfb
Thenuka Gunawardana <thenukanisal@gmail.com>
2022-04-30 09:09:37 +0530

    Create React

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+const data=
+{
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            catergory:'Shirts',
+            image:'/images/p1.jpg',
+            price:49,
+            brand:'Nike',
+            ratung:4.5,
+            numReviews:12,
+            description:'Original Nike Tshirt',
+        },
+
+        {
+            _id:'2',
+            name:'Nike Slim short',
+            catergory:'Shorts',
+            image:'/images/p2.jpg',
+            price:69,
+            brand:'Nike',
+            ratung:4.7,
+            numReviews:21,
+            description:'Original Nike short',
+        },
+
+        {
+            _id:'3',
+            name:'Adidas T-Shirt Black',
+            catergory:'Shirts',
+            image:'/images/p3.jpg',
+            price:75,
+            brand:'Nike',
+            ratung:4.5,
+            numReviews:13,
+            description:'Original Adidas Tshirt',
+        },
+
+        {
+            _id:'4',
+            name:'Nike Air Force 1',
+            catergory:'Shoes',
+            image:'/images/p4.jpg',
+            price:400,
+            brand:'Nike',
+            ratung:4.5,
+            numReviews:10,
+            description:'Original Nike Air Force 1',
+        },
+
+        {
+            _id:'5',
+            name:'Adidas slides',
+            catergory:'Shoes',
+            image:'/images/p5.jpg',
+            price:250,
+            brand:'Nike',
+            ratung:4.5,
+            numReviews:10,
+            description:'Adidas outdoor slides',
+        },
+
+        {
+            _id:'6',
+            name:'Nike Tshirt (Women"s)',
+            catergory:'Shirts',
+            image:'/images/p6.jpg',
+            price:59,
+            brand:'Nike',
+            ratung:4.5,
+            numReviews:10,
+            description:'Original Nike Tshirt for women',
+        },
+    ],
+};
+export default data;
