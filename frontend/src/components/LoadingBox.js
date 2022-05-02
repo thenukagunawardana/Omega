9c2545d7f03d2f9107b3fcfb4c0904271aa1e549
Thenuka Gunawardana <thenukanisal@gmail.com>
2022-05-02 11:37:41 +0530

    Fetching data from backend to Frontend

---
+++ frontend/src/components/LoadingBox.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div>
+        <i className="fa fa-spinner fa- spinner"></i> Loading.....
+    </div>
+  )
+}
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -49,7 +49,7 @@ export default function ProductScreen(props) {
                     {product.countInStock > 0 ? (
                       <span className="success">In Stock</span>
                     ) : (
-                      <span className="error">Unavailable</span>
+                      <span className="damger">Unavailable</span>
                     )}
                   </div>
                 </div>
