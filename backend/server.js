7c469a83dd91bed55b8be621a2275931da2653bf
Thenuka Gunawardana <thenukanisal@gmail.com>
2022-05-02 13:54:59 +0530

    Added Redux

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,17 @@
 import express from "express";
 import data from "./data.js";
 
-const app=express();
+const app = express();
 
-app.get('/api/products',(req,res)=>
-{
-    res.send(data.products);
+app.get("/api/products", (req, res) => {
+	res.send(data.products);
 });
 
-
-app.get('/',(req,res) =>
-{
-    res.send('Server is ready');
+app.get("/", (req, res) => {
+	res.send("Server is ready");
 });
 
-const port=process.env.PORT || 5000;
-app.listen(5000, () =>
-    {
-        console.log(`Serve at http://localhost:${port}`);
-    });
+const port = process.env.PORT || 4000;
+app.listen(4000, () => {
+	console.log(`Serve at http://localhost:${port}`);
+});
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,48 +1,31 @@
-import React from 'react';
-import axios from 'axios';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import Product from '../components/Product';
-import MessageBox from '../components/MessageBox';
-import LoadingBox from '../components/LoadingBox';
-
+import React from "react";
+import { useEffect } from "react";
+import Product from "../components/Product";
+import MessageBox from "../components/MessageBox";
+import LoadingBox from "../components/LoadingBox";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
 export default function HomeScreen() {
-  
-  const [products, setProducts]=useState([]);
+	const dispatch = useDispatch();
+	const productList = useSelector((state) => state.productList);
+	const { loading, error, products } = productList;
+	useEffect(() => {
+		dispatch(listProducts());
+	}, [dispatch]);
 
-  const [loading,setLoading]= useState(false);
-
-  const [error, setError]=useState(false);
-
-  useEffect(()=>{
-    const fetchData=async()=>
-    {
-      try{
-        setLoading(true);
-        const {data} = await axios.get('/api/products');
-        setLoading(false);
-        setProducts(data);
-      } catch (err)
-      {
-        setError(err.message);
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, [])
-
-  return (
-    <div>
-      {loading? ( <LoadingBox></LoadingBox>
-      ):
-      error? ( <MessageBox variant="danger">{error}</MessageBox>
-      ):(
-      <div className="row center">
-        {products.map((product) => (
-          <Product key={product._id} product={product}></Product>
-        ))}
-      </div>
-      )} 
-    </div>
-  );
+	return (
+		<div>
+			{loading ? (
+				<LoadingBox></LoadingBox>
+			) : error ? (
+				<MessageBox variant="danger">{error}</MessageBox>
+			) : (
+				<div className="row center">
+					{products.map((product) => (
+						<Product key={product._id} product={product}></Product>
+					))}
+				</div>
+			)}
+		</div>
+	);
 }
