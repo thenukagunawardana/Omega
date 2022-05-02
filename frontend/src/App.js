3492fed4f6488a18ee591d4707a042e55d09a3ff
Thenuka Gunawardana <thenukanisal@gmail.com>
2022-05-02 21:52:01 +0530

    Handle Add to Cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import CartScreen from "./screens/CartScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="grid-container">
-        <header className="row">
-          <div>
-            <a className="brand" href="/">
-              Omega
-            </a>
-          </div>
-          <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
-          </div>
-        </header>
-        <main>
-          <Route path="/product/:id" component={ProductScreen}></Route>
-          <Route path="/" component={HomeScreen} exact></Route>
-        </main>
-        <footer className="row center">All right reserved</footer>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className="grid-container">
+				<header className="row">
+					<div>
+						<a className="brand" href="/">
+							Omega
+						</a>
+					</div>
+					<div>
+						<a href="/cart">Cart</a>
+						<a href="/signin">Sign In</a>
+					</div>
+				</header>
+				<main>
+					<Route path="/cart/:id?" component={CartScreen}></Route>
+					<Route path="/product/:id" component={ProductScreen}></Route>
+					<Route path="/" component={HomeScreen} exact></Route>
+				</main>
+				<footer className="row center">All right reserved</footer>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,66 +1,121 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Rating from '../components/Rating';
-import data from '../data';
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "../components/Rating";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { useSelector, useDispatch } from "react-redux";
+import { detailsProduct } from "../actions/productActions";
+import { useEffect } from "react/cjs/react.development";
+import { useState } from "react/cjs/react.development";
 
 export default function ProductScreen(props) {
-  const product = data.products.find((x) => x._id === props.match.params.id);
-  if (!product) {
-    return <div> Product Not Found</div>;
-  }
-  return (
-    <div>
-      <Link to="/">Back to result</Link>
-      <div className="row top">
-        <div className="col-2">
-          <img className="large" src={product.image} alt={product.name}></img>
-        </div>
-        <div className="col-1">
-          <ul>
-            <li>
-              <h1>{product.name}</h1>
-            </li>
-            <li>
-              <Rating
-                rating={product.rating}
-                numReviews={product.numReviews}
-              ></Rating>
-            </li>
-            <li>Price : ${product.price}</li>
-            <li>
-              Description:
-              <p>{product.description}</p>
-            </li>
-          </ul>
-        </div>
-        <div className="col-1">
-          <div className="card card-body">
-            <ul>
-              <li>
-                <div className="row">
-                  <div>Price</div>
-                  <div className="price">${product.price}</div>
-                </div>
-              </li>
-              <li>
-                <div className="row">
-                  <div>Status</div>
-                  <div>
-                    {product.countInStock > 0 ? (
-                      <span className="success">In Stock</span>
-                    ) : (
-                      <span className="damger">Unavailable</span>
-                    )}
-                  </div>
-                </div>
-              </li>
-              <li>
-                <button className="primary block">Add to Cart</button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const dispatch = useDispatch();
+	const productId = props.match.params.id;
+	const [qty, setQty] = useState(1);
+	const productDetails = useSelector((state) => state.productDetails);
+	const { loading, error, product } = productDetails;
+
+	useEffect(() => {
+		dispatch(detailsProduct(productId));
+	}, [dispatch, productId]);
+
+	const addToCartHandler = () => {
+		props.history.push(`/cart/${productId}?qty=${qty}`);
+	};
+
+	return (
+		<div>
+			{loading ? (
+				<LoadingBox></LoadingBox>
+			) : error ? (
+				<MessageBox variant="danger">{error}</MessageBox>
+			) : (
+				<div>
+					<Link to="/">Back to result</Link>
+					<div className="row top">
+						<div className="col-2">
+							<img
+								className="large"
+								src={product.image}
+								alt={product.name}
+							></img>
+						</div>
+						<div className="col-1">
+							<ul>
+								<li>
+									<h1>{product.name}</h1>
+								</li>
+								<li>
+									<Rating
+										rating={product.rating}
+										numReviews={product.numReviews}
+									></Rating>
+								</li>
+								<li>Price : ${product.price}</li>
+								<li>
+									Description:
+									<p>{product.description}</p>
+								</li>
+							</ul>
+						</div>
+						<div className="col-1">
+							<div className="card card-body">
+								<ul>
+									<li>
+										<div className="row">
+											<div>Price</div>
+											<div className="price">${product.price}</div>
+										</div>
+									</li>
+									<li>
+										<div className="row">
+											<div>Status</div>
+											<div>
+												{product.countInStock > 0 ? (
+													<span className="success">In Stock</span>
+												) : (
+													<span className="danger">Unavailable</span>
+												)}
+											</div>
+										</div>
+									</li>
+									{product.countInStock > 0 && (
+										<>
+											<li>
+												<div className="row">
+													<div>Qty</div>
+													<div>
+														<select
+															value={qty}
+															onChange={(e) => setQty(e.target.value)}
+														>
+															{[...Array(product.countInStock).keys()].map(
+																(x) => (
+																	<option key={x + 1} value={x + 1}>
+																		{x + 1}
+																	</option>
+																)
+															)}
+														</select>
+													</div>
+												</div>
+											</li>
+											<li>
+												<button
+													onClick={addToCartHandler}
+													className="primary block"
+												>
+													Add to Cart
+												</button>
+											</li>
+										</>
+									)}
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
