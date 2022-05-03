bf3e90b6fa1e312f6542ebbce7c7b39621110fd6
Thenuka Gunawardana <thenukanisal@gmail.com>
2022-05-03 15:08:34 +0530

    Create MongoDB connection and Adding users

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 import express from "express";
+import mongoose from "mongoose";
 import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
+mongoose.connect("mongodb://localhost/omega", {});
 
 app.get("/api/products/:id", (req, res) => {
 	const product = data.products.find((x) => x._id === req.params.id);
@@ -17,8 +20,14 @@ app.get("/api/products", (req, res) => {
 	res.send(data.products);
 });
 
+app.use("/api/users", userRouter);
+
 app.get("/", (req, res) => {
 	res.send("Server is ready");
+});
+
+app.use((err, req, res, next) => {
+	res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 4000;
