20578f28d34aec534cd0e3ba76a4c7d63cb81a07
Thenuka Gunawardana <thenukanisal@gmail.com>
2022-05-03 09:19:34 +0530

    Implementation of Add to cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react/cjs/react.development";
+import { addToCart } from "../actions/cartActions";
 
 export default function CartScreen(props) {
 	const productId = props.match.params.id;
 	const qty = props.location.search
 		? Number(props.location.search.split("=")[1])
 		: 1;
+	const dispatch = useDispatch();
+	useEffect(() => {
+		if (productId) {
+			dispatch(addToCart(productId, qty));
+		}
+	}, [dispatch, productId, qty]);
+
 	return (
 		<div>
 			<h1>Cart Screen</h1>
 			<p>
-				Add to Cart:ProductID:{productId} Qty:{qty}
+				Add to Cart : ProductID : {productId} Qty : {qty}
 			</p>
 		</div>
 	);
