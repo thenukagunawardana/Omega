65b8c64ce279e9b2ad3ea30fd2bdb6dcb88f2855
Thenuka Gunawardana <thenukanisal@gmail.com>
2022-05-05 09:03:56 +0530

    Payment Methods and Payment Screen

---
+++ frontend/src/screens/PaymentMethodScreen.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { useState } from "react";
+import CheckoutSteps from "../components/CheckoutSteps";
+import { useDispatch, useSelector } from "react-redux";
+import { savePaymentMethod } from "../actions/cartActions";
+
+export default function PaymentMethodScreen(props) {
+	const cart = useSelector((state) => state.cart);
+	const { shippingAddress } = cart;
+	if (!shippingAddress.address) {
+		props.history.push("/shipping");
+	}
+	const [paymentMethod, setPaymentMethod] = useState("PayPal");
+	const dispatch = useDispatch();
+	const submitHandler = (e) => {
+		e.preventDefault();
+		dispatch(savePaymentMethod(paymentMethod));
+		props.history.push("/placeorder");
+	};
+	return (
+		<div>
+			<CheckoutSteps step1 step2 step3></CheckoutSteps>
+			<form className="form" onSubmit={submitHandler}>
+				<div>
+					<h1>Payment Method</h1>
+				</div>
+				<div>
+					<div>
+						<input
+							type="radio"
+							id="paypal"
+							value="PayPal"
+							name="paymentMethod"
+							required
+							checked
+							onChange={(e) => setPaymentMethod(e.target.value)}
+						></input>
+						<label htmlFor="paypal">PayPal</label>
+					</div>
+				</div>
+				<div>
+					<div>
+						<input
+							type="radio"
+							id="cash"
+							value="Cash"
+							name="paymentMethod"
+							required
+							onChange={(e) => setPaymentMethod(e.target.value)}
+						></input>
+						<label htmlFor="cash">Cash On Delivery</label>
+					</div>
+				</div>
+				<div>
+					<label />
+					<button className="primary" type="submit">
+						Continue
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+}
